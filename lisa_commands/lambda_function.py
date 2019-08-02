03bcf389e8380dcf124901ea2adf212e76cad718
Splyth <Splyth@gmail.com>
2019-08-02 16:09:34 -0500

    Update lambda_function.py

--- lisa_commands/lambda_function.py
+++ lisa_commands/lambda_function.py
@@ -26,7 +26,6 @@ def lambda_handler(data, context):
         'channel': channel_id,
         'text': text
     }
-    
-    logging.warn(data)
+
     request.submit_slack_request(json.dumps(data).encode('utf-8'))
     return request.return_status()
