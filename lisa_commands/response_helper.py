34ffa572c576a3afbacd052c164649c1db9a795e
Splyth <Splyth@gmail.com>
2020-07-19 18:17:28 -0500

    oops wrong holdiay

--- lisa_commands/response_helper.py
+++ lisa_commands/response_helper.py
@@ -31,7 +31,7 @@ def apply_additional_message_options(body):
     # During October for Halloween
     elif now.month == 10:
         body['username'] ='Witchy Lisa'
-        body["icon_url"] = 'https://raw.githubusercontent.com/Splyth/slack_bot/master/icons/Lisa_Easter.png'
+        body["icon_url"] = 'https://raw.githubusercontent.com/Splyth/slack_bot/master/icons/Lisa_Halloween.png'
     # During December for Christmas
     elif now.month == 12:
         body['username'] ='Santa Lisa'
